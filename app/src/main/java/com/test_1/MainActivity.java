db2c1ecb9ff06418c9594e113721db5a1f0078b8
PromiseMiss <1061520515@qq.com>
2021-04-01 17:30:42 +0800

    提交

--- app/src/main/java/com/test_1/MainActivity.java
+++ app/src/main/java/com/test_1/MainActivity.java
@@ -4,11 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.ashokvarma.bottomnavigation.BottomNavigationBar;
+import com.ashokvarma.bottomnavigation.BottomNavigationItem;
+import com.hjd.applib.base.BaseBindingActivity;
+import com.test_1.databinding.ActivityMainBinding;
+
+public class MainActivity extends BaseBindingActivity<ActivityMainBinding> {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+    public void initView() {
+        binding.bottomNavigationBar.setMode(BottomNavigationBar.MODE_FIXED);
+        binding.bottomNavigationBar.setBackgroundStyle(BottomNavigationBar.BACKGROUND_STYLE_STATIC);
+        binding.bottomNavigationBar.setBarBackgroundColor(R.color.white);
+
+        binding.bottomNavigationBar
+                .addItem(new BottomNavigationItem(R.mipmap.ic_launcher, "首页").setActiveColorResource(R.color.main_color).setInactiveIconResource(R.mipmap.ic_launcher).setInActiveColorResource(R.color.color_f6))
+                .addItem(new BottomNavigationItem(R.mipmap.ic_launcher, "资讯").setActiveColorResource(R.color.main_color).setInactiveIconResource(R.mipmap.ic_launcher).setInActiveColorResource(R.color.color_f6))
+                .addItem(new BottomNavigationItem(R.mipmap.ic_launcher, "我的").setActiveColorResource(R.color.main_color).setInactiveIconResource(R.mipmap.ic_launcher).setInActiveColorResource(R.color.color_f6))
+                .setFirstSelectedPosition(0)
+                .initialise();
+
     }
 }
